998947e8cd9fbd4b68027a0e3f495965ed611611
John Stewart <jstewartstack@gmail.com>
2022-02-18 13:10:42 +0000

    piggybnkprj

---
+++ piggybank.swift
@@ -0,0 +1,17 @@
+var pesos: Double = 4
+var reais: Double = 5
+var soles: Double = 6
+
+var total: Double = 0
+
+//print(total)
+
+/*Converions
+pesos = 0.049 usd
+reais = 0.17  usd
+soles = 0.27  usd
+*/
+
+total = 0.049 * pesos + 0.19 * reais + 0.27 * soles 
+
+print("Total USD = \(total).")
