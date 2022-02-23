64da5fcf34aeaa62b3e531fc0d7156a561009cc5
John Stewart <jstewartstack@gmail.com>
2022-02-23 12:50:27 +0000

    whaletalkprojswift

---
+++ whaletalk.swift
@@ -0,0 +1,23 @@
+var input = "Hi there my name is John. I am learning swift on Codecademy!"
+
+var output = ""
+
+for char in input {
+  let lowerChar = char.lowercased()
+
+switch lowerChar {
+  case "a", "i", "o", "j", "h":
+    output += lowerChar.uppercased()
+  case "e":
+    output += "EE"
+  case "u":
+    output += "UU"
+  case "d":
+    output += "BLUB"
+  default:
+    continue
+
+ }
+}
+
+print(output)
