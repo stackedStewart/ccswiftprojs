5723eaf7c474196c57fe125db79bbbb4fea82959
John Stewart <jstewartstack@gmail.com>
2022-02-24 14:22:26 +0000

    99bottles

---
+++ 99bottles.swift
@@ -0,0 +1,10 @@
+var numBottles: Int = 99
+
+while numBottles > 0 {
+  print("\(numBottles) of milk on the wall, \(numBottles) bottles of milk!") 
+  print("You take one down pass it around...") 
+  numBottles -= 1
+  print("\(numBottles) bottles of milk on the wall!")
+}
+
+print("0 bottles of milk on the wall, 0 bottles of milk! Go to the store buy some more, 99 bottles of milk on the wall!")
