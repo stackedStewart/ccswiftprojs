1fe5c2d6cd2a8b2675cb6ebd1fdf157c04e87f24
John Stewart <jstewartstack@gmail.com>
2022-03-02 10:00:59 +0000

    oddevenarray

---
+++ oddevenoptional.swift
@@ -0,0 +1,16 @@
+var list = [2, 4, 3, 6, 1, 9]
+
+// Write your code below 🧮
+var totalEven = 0
+var productOdd = 1
+
+for item in list {
+  if item % 2 != 0 {
+    productOdd *= item
+  } else {
+    totalEven += item
+  }
+}
+
+print("Product of odd is \(productOdd)")
+print("Sum of even is \(totalEven)")
