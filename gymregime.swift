0a642ef4c97bdd39f1166842bf536683de922d88
John Stewart <jstewartstack@gmail.com>
2022-03-05 09:49:07 +0000

    gymcommit

---
+++ gymregime.swift
@@ -0,0 +1,48 @@
+// Write your code below 💪
+struct Exercise {
+  var name: String
+  var muscleGroups: [String]
+  var reps: Int
+  var sets: Int
+  var totalReps: Int
+
+  init(name: String, muscleGroups: [String], reps: Int, sets: Int) {
+    self.name = name
+    self.muscleGroups = muscleGroups
+    self.reps = reps
+    self.sets = sets
+    self.totalReps = reps * sets
+  }
+}
+
+//Adding Exercise Instances
+var pushUp = Exercise(name: "Push up", muscleGroups: ["Triceps", "Chest", "Shoulders"], reps: 10, sets: 3)
+
+//print(pushUp)
+
+//Creating The Regimen Structure
+struct Regimen {
+  var dayOfWeek: String
+  var exercises: [Exercise]
+
+  init(dayOfWeek: String, exercises: [Exercise]) {
+    self.dayOfWeek = dayOfWeek
+    self.exercises = exercises
+  }
+
+  func printExercisePlan() {
+    print("Today is \(self.dayOfWeek) and the plan is to: ")
+    for exercise in self.exercises {
+    print("Do \(exercise.sets) sets of \(exercise.reps) \(exercise.name)s")
+    print("That's a total of \(exercise.totalReps) \(exercise.name).")
+  }
+ }
+}
+
+var mondayRegimen = Regimen(dayOfWeek: "Monday", exercises:[pushUp])
+
+//print(mondayRegimen)
+
+mondayRegimen.printExercisePlan()
+
+//John Stewart | Codecademy | 5/3/22
